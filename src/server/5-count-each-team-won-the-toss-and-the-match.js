5cf910ac9b51ef909ce6ce00326ab7bc4d022af0
NK1FC <nk987tk987@gmail.com>
2023-03-15 17:42:40 +0530

    new file

---
+++ src/server/5-count-each-team-won-the-toss-and-the-match.js
@@ -0,0 +1,35 @@
+const { matchesData } = require('./csvreader'); // Importing Matches data
+const writeFile = require('./file-writer'); // Importing Function to write File
+const path = require('path');
+
+
+function countWonTossAndMatchPerTeam(matches) {
+    try {
+        let countWonTossAndMatchEachTeam = matches.reduce((acc, match) => {
+
+            let toss_winner = match.toss_winner;
+            let winner = match.winner;
+            if (winner !== '' && toss_winner !== '') {
+                acc[winner] = (acc[winner] || 0) + (winner === toss_winner ? 1 : 0);
+            }
+            return acc;
+        }, {});
+        return countWonTossAndMatchEachTeam;
+    } catch (err) {
+        console.log(err);
+        return {};
+    }
+}
+
+
+// Executing the Code
+
+let OutputPath = path.resolve(__dirname, '..', 'public', '5-count-each-team-won-the-toss-and-the-match.json');
+
+matchesData.then((matches) => {
+
+    const countWonTossAndMatchEachTeam= countWonTossAndMatchPerTeam(matches);  // Counting the matches per year
+    console.log(countWonTossAndMatchEachTeam);
+    writeFile(OutputPath, countWonTossAndMatchEachTeam);  // Writing the output to a JSON file
+
+});
